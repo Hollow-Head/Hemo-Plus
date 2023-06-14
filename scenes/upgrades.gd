bcb750e01466a8fd0a8230bb0f4918c29096edc1
Aloísio M. Lingo Filho <alingofilho@gmail.com>
2023-06-14 10:39:44 -0300

    Assets

---
+++ scenes/upgrades.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var list = [{
+	"name": "Campanha",
+	"img": "res://assets/sprites/campanha.jpg"}
+, 	{"name": "CU",
+	"img": "res://assets/sprites/button.png"
+}]
---
+++ scenes/variables.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var blood
+var multiplier
+
+func _ready():
+	blood = 0
+	multiplier = 1
+
+func _process(delta):
+	pass
