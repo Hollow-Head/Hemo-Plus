f3afefc32116fdcac9113e8b345b3ef045fdfa05
Aloísio M. Lingo Filho <alingofilho@gmail.com>
2023-06-14 16:41:27 -0300

    MainScene Pt.1

---
+++ scenes/Main.gd
@@ -0,0 +1,9 @@
+extends VBoxContainer
+
+@onready var button = $Button
+@onready var bloodText = $BloodText
+
+func _on_clicked():
+	Variables.blood += 1
+	bloodText.text = str(Variables.blood)
+	pass # Replace with function body.
--- scenes/main.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@onready var button = $Button
-@onready var bloodText = $BloodText
-
-func _on_clicked():
-	Variables.blood += 1
-	bloodText.text = str(Variables.blood)
-	pass # Replace with function body.
