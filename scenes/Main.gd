736b18b7fd7019a234decd1f0cc60f9d4deaac2d
Aloísio M. Lingo Filho <alingofilho@gmail.com>
2023-06-15 14:56:10 -0300

    update Main.gd

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -1,9 +1,8 @@
 extends VBoxContainer
 
-@onready var button = $Button
-@onready var bloodText = $BloodText
+@onready var button = $Heart/HeartButton
+@onready var bloodText = $Stats/CenterContainer2/BloodText
 
-func _on_clicked():
+func _on_heart_button_pressed():
 	Variables.blood += 1
 	bloodText.text = str(Variables.blood)
-	pass # Replace with function body.
