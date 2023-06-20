0497a528f0fb2cb8c23730f26751d8801f60173c
Aloísio M. Lingo Filho <alingofilho@gmail.com>
2023-06-20 13:08:27 -0300

    uppanel

--- scenes/upgrade_item.gd
+++ scenes/upgrade_item.gd
@@ -3,10 +3,13 @@ extends VBoxContainer
 @onready var icon = $MarginContainer/HBoxContainer/Icon
 @onready var desc = $MarginContainer/HBoxContainer/VBoxContainer/Description
 @onready var Uname = $MarginContainer/HBoxContainer/VBoxContainer/Name
-@onready var price = $MarginContainer/HBoxContainer/BuyButton/VBoxContainer/HBoxContainer/BloodPrice
-@onready var peoplePrice = $MarginContainer/HBoxContainer/BuyButton/VBoxContainer/HBoxContainer2/PeoplePrice
-@onready var peopleIcon = $MarginContainer/HBoxContainer/BuyButton/VBoxContainer/HBoxContainer2/PeopleIcon
-
+@onready var bloodPrice = $MarginContainer/HBoxContainer/BuyButton/BuyContainer/Blood/BloodPrice
+@onready var bloodIcon = $MarginContainer/HBoxContainer/BuyButton/BuyContainer/Blood/BloodIcon
+@onready var peoplePrice = $MarginContainer/HBoxContainer/BuyButton/BuyContainer/People/PeoplePrice
+@onready var peopleIcon = $MarginContainer/HBoxContainer/BuyButton/BuyContainer/People/PeopleIcon
+@onready var buyContainer = $MarginContainer/HBoxContainer/BuyButton/BuyContainer
+@onready var people = $MarginContainer/HBoxContainer/BuyButton/BuyContainer/People
+@onready var blood = $MarginContainer/HBoxContainer/BuyButton/BuyContainer/Blood
 var things = {}
 
 func atualizar():
@@ -14,9 +17,15 @@ func atualizar():
 	icon.size = Vector2(168,168)
 	Uname.text = things.name
 	desc.text = things.description
-	price.text = things.blood_price
+	bloodPrice.text = things.blood_price	
+	peoplePrice.text = things.people_price
 	
-	if (things.has("peoplePrice")):
-		print(things.peoplePrice)
+	if (things.blood_price == ""):
+		buyContainer.remove_child(blood)
+		
+				
+	if (things.people_price == ""):
+		buyContainer.remove_child(people)
+		
 	
 	
--- scenes/upgrades.gd
+++ scenes/upgrades.gd
@@ -2,31 +2,29 @@ extends Node
 
 var list = [{
 	"name": "Campanha",
-	"blood_price": "10",
+	"blood_price": "",
 	"people_price": "10",
 	"description": "\"Realizar Campanhas pela cidade.\"\nAumenta 0.1 Sangues Por Segundo.",
 	"img": "res://icon.svg",
-	}, {
-	"name": "Curar o Câncer",
-	"blood_price": "10",
-	"description": "\"Cura o Câncer e salva o mundo top 👍.\"",
-	"img": "res://assets/sprites/pato.jpg",
 	},
 	{
 	"name": "Campanha",
 	"blood_price": "10",
+	"people_price": "",
 	"description": "\"Realizar Campanhas pela cidade.\"",
 	"img": "res://icon.svg",
 	},
 	{
 	"name": "Campanha",
 	"blood_price": "10",
+	"people_price": "",
 	"description": "\"Realizar Campanhas pela cidade.\"",
 	"img": "res://icon.svg",
 	}, 
 	{
 	"name": "Campanha",
 	"blood_price": "10",
+	"people_price": "",
 	"description": "\"Realizar Campanhas pela cidade.\"",
 	"img": "res://icon.svg",
 	}]
